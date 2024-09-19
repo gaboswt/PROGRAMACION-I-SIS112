74a193f37b8903fb63a6d82f50c7c24f955a745c
gaboswt <enilobag321@gmail.com>
2024-09-18 17:10:44 -0700

    16/9

--- codigo/practicas/palsecre.py
+++ codigo/practicas/palsecre.py
@@ -1,9 +1,11 @@
 import random
 lista=["hola", "adios", "progra", "ucb", "biomedica"]
+
 palsec = random.choice(lista)
 intentos = 10
 palnoencontrada = ["_"] * len(palsec)
 print(palnoencontrada)
+
 for i in range (intentos):
     new = input("Ingresa una letra: ")
     for j in range(len(palsec)):
@@ -11,6 +13,6 @@ for i in range (intentos):
             palnoencontrada[j]=new
     print(palnoencontrada)
     if "_" not in palnoencontrada:
-        print("¡FELCIDADES, encontraste la palabra!")
+        print("¡FELICIDADES, encontraste la palabra!")
 else:
     print(f"Te quedaste sin intentos. La palabra era: {palsec}")
